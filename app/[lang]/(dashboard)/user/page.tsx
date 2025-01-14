86b3b0817811b0f53f9976f22cf1d508cc86d12d
Abdullah Saber <abdullah.saber.mustafa@.com>
2025-01-14 18:13:05 +0200

    fix conflict

--- app/[lang]/(dashboard)/user/page.tsx
+++ app/[lang]/(dashboard)/user/page.tsx
@@ -3,12 +3,12 @@ import React from "react";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 
 import TableData from "./TableData";
-import BreadcrumbComponent from "../shared/BreadcrumbComponent";
 import { useTranslate } from "@/config/useTranslation";
 import Create from "@/components/common/create/Create";
 import { requiredFieldsUser } from "@/config/columnsRequired";
 import { Button } from "@/components/ui/button";
 import { Icon } from "@iconify/react";
+import BreadcrumbComponent from "../(category-mangement)/shared/BreadcrumbComponent";
 const page = () => {
   const { t, loading, error } = useTranslate();
 
