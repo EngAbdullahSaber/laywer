81384ecabe86f22371b3e1b7b7a854d0f07e3dbe
EngAbdullahSaber <abdullah.saber.mustafa@gmail.com>
2025-02-26 19:33:34 +0200

    fifnish

--- app/[lang]/(dashboard)/(admin-managment)/case/View.tsx
+++ app/[lang]/(dashboard)/(admin-managment)/case/View.tsx
@@ -74,8 +74,8 @@ const ViewMore: React.FC<ViewUserData> = ({ row }) => {
   //     </>
   //   );
   // };
-  const renderClientsData = () => {
-    const casesData = row?.original;
+  const renderLawyerData = () => {
+    const casesData = row?.original?.lawyer;
 
     return (
       <>
@@ -85,42 +85,24 @@ const ViewMore: React.FC<ViewUserData> = ({ row }) => {
           animate={{ opacity: 1 }}
           transition={{ duration: 0.5 }}
         >
-          {t("Client Info")}
+          {t("Lawyer Info")}
         </motion.h3>
         <ul className="md:grid grid-cols-2 !mt-5 gap-2 space-y-2 md:space-y-0">
           <DetailItem label={t("Id")} value={casesData?.id || "-"} />
           <DetailItem label={t("name")} value={casesData?.name || "-"} />
           <DetailItem label={t("email")} value={casesData?.email || "-"} />
-          <DetailItem label={t("phone")} value={casesData?.phone || "-"} />
-          <DetailItem label={t("details")} value={casesData?.details || "-"} />
-          <DetailItem
-            label={t("client_type")}
-            value={casesData?.client_type || "-"}
-          />
           <DetailItem label={t("address")} value={casesData?.address || "-"} />
-
+          <DetailItem label={t("phone")} value={casesData?.phone || "-"} />
           <DetailItem
-            label={t("client_files")}
-            value={casesData?.client_files || "-"}
-          />
-          <DetailItem
-            label="Date Of Create"
-            value={
-              new Date(casesData?.created_at).toLocaleDateString("en-GB") || "-"
-            }
-          />
-          <DetailItem
-            label="Date Of Create"
-            value={
-              new Date(casesData?.updated_at).toLocaleDateString("en-GB") || "-"
-            }
+            label={t("category")}
+            value={casesData?.category.name || "-"}
           />
         </ul>
       </>
     );
   };
   const renderCourtsData = () => {
-    const casesData = row?.original;
+    const casesData = row?.original?.court;
 
     return (
       <>
@@ -161,10 +143,6 @@ const ViewMore: React.FC<ViewUserData> = ({ row }) => {
             value={casesData?.room_number || "-"}
           />
           <DetailItem
-            label={t("client_files")}
-            value={casesData?.client_files || "-"}
-          />
-          <DetailItem
             label="Date Of Create"
             value={
               new Date(casesData?.created_at).toLocaleDateString("en-GB") || "-"
@@ -177,6 +155,54 @@ const ViewMore: React.FC<ViewUserData> = ({ row }) => {
             }
           />
         </ul>
+      </>
+    );
+  };
+  const renderCaseFilesData = () => {
+    const lawyerData = row?.original?.files;
+    console.log(typeof lawyerData);
+    // If lawyerData is not an array or is empty, return a fallback message
+    if (!Array.isArray(lawyerData) || lawyerData.length === 0) {
+      return (
+        <>
+          <motion.h3
+            className="font-semibold text-lg"
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ duration: 0.5 }}
+          >
+            {t("Client Files")}
+          </motion.h3>
+          <p className="text-gray-500 mt-2">{t("No Files found")}</p>
+        </>
+      );
+    }
+
+    return (
+      <>
+        <div className="space-y-4 mt-5">
+          {lawyerData.map((file, index) => (
+            <motion.li
+              key={index}
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              transition={{ delay: 0.3, duration: 0.5 }}
+              className="flex flex-row gap-6 items-center"
+            >
+              <span className="text-sm text-default-900 font-medium w-[52%]">
+                {t("Client Files")}:
+              </span>
+              <a
+                href={file.url} // Access the file URL dynamically from the `file` object
+                className="text-default-500 font-semibold w-[40%]"
+                target="_blank"
+                rel="noopener noreferrer" // Added for security when opening links
+              >
+                {t("Show File")} {/* Display the file name */}
+              </a>
+            </motion.li>
+          ))}
+        </div>
       </>
     );
   };
@@ -200,10 +226,7 @@ const ViewMore: React.FC<ViewUserData> = ({ row }) => {
             label={t("category")}
             value={casesData?.category.name || "-"}
           />
-          <DetailItem
-            label={t("case_numbers")}
-            value={casesData?.case_numbers.case_number_id || "-"}
-          />
+
           <DetailItem
             label={t("main_case_number")}
             value={casesData?.main_case_number || "-"}
@@ -232,23 +255,24 @@ const ViewMore: React.FC<ViewUserData> = ({ row }) => {
           />
           <DetailItem label={t("details")} value={casesData?.details || "-"} />
           {/* <DetailItem
-            label="Date Of Create Account"
-            value={
-              new Date(casesData?.created_at).toLocaleDateString("en-GB") || "-"
-            }
-          />
-          <DetailItem
-            label="Last Update of Account"
-            value={
-              new Date(casesData?.updated_at).toLocaleDateString("en-GB") || "-"
-            }
-          /> */}
+             label="Date Of Create Account"
+             value={
+               new Date(casesData?.created_at).toLocaleDateString("en-GB") || "-"
+             }
+           />
+           <DetailItem
+             label="Last Update of Account"
+             value={
+               new Date(casesData?.updated_at).toLocaleDateString("en-GB") || "-"
+             }
+           /> */}
         </ul>
+        {renderCaseFilesData()}
       </>
     );
   };
-  const renderLawyerData = () => {
-    const casesData = row?.original;
+  const renderClientsData = () => {
+    const casesData = row?.original?.client;
 
     return (
       <>
@@ -258,17 +282,30 @@ const ViewMore: React.FC<ViewUserData> = ({ row }) => {
           animate={{ opacity: 1 }}
           transition={{ duration: 0.5 }}
         >
-          {t("Lawyer Info")}
+          {t("Client Info")}
         </motion.h3>
         <ul className="md:grid grid-cols-2 !mt-5 gap-2 space-y-2 md:space-y-0">
           <DetailItem label={t("Id")} value={casesData?.id || "-"} />
           <DetailItem label={t("name")} value={casesData?.name || "-"} />
           <DetailItem label={t("email")} value={casesData?.email || "-"} />
-          <DetailItem label={t("address")} value={casesData?.address || "-"} />
           <DetailItem label={t("phone")} value={casesData?.phone || "-"} />
           <DetailItem
-            label={t("category")}
-            value={casesData?.category.name || "-"}
+            label={t("client_type")}
+            value={casesData?.client_type || "-"}
+          />
+          <DetailItem label={t("address")} value={casesData?.address || "-"} />
+
+          <DetailItem
+            label="Date Of Create"
+            value={
+              new Date(casesData?.created_at).toLocaleDateString("en-GB") || "-"
+            }
+          />
+          <DetailItem
+            label="Date Of Create"
+            value={
+              new Date(casesData?.updated_at).toLocaleDateString("en-GB") || "-"
+            }
           />
         </ul>
       </>
--- app/[lang]/(dashboard)/(admin-managment)/lawyer/View.tsx
+++ app/[lang]/(dashboard)/(admin-managment)/lawyer/View.tsx
@@ -58,90 +58,80 @@ const ViewMore: React.FC<ViewUserData> = ({ row }) => {
           {t("Profile Data")}
         </motion.h3>
         <ul className="md:grid grid-cols-2 !mt-5 gap-2 space-y-2 md:space-y-0">
-          <motion.li
-            initial={{ opacity: 0 }}
-            animate={{ opacity: 1 }}
-            transition={{ delay: 0.3, duration: 0.5 }}
-            className="flex flex-row gap-6 items-center"
-          >
-            <span className="text-sm text-default-900 font-medium w-[52%]">
-              {t("Lawyer Licence Photo")}:
-            </span>
-            <span className="text-default-500 font-semibold w-[40%]">
-              <Avatar className="w-16 h-16">
-                <AvatarImage
-                  className="w-16 h-16"
-                  src={`${profile?.lawyer_licence?.url}`}
-                />
-                <AvatarFallback>
-                  {profile?.name?.slice(0, 2).toUpperCase()}
-                </AvatarFallback>
-              </Avatar>
-            </span>
-          </motion.li>
-          <motion.li
-            initial={{ opacity: 0 }}
-            animate={{ opacity: 1 }}
-            transition={{ delay: 0.3, duration: 0.5 }}
-            className="flex flex-row gap-6 items-center"
-          >
-            <span className="text-sm text-default-900 font-medium w-[52%]">
-              {t("Driving Licence Photo")}:
-            </span>
-            <span className="text-default-500 font-semibold w-[40%]">
-              <Avatar className="w-16 h-16">
-                <AvatarImage
-                  className="w-16 h-16"
-                  src={`${profile?.driving_licence?.url}`}
-                />
-                <AvatarFallback>
-                  {profile?.name?.slice(0, 2).toUpperCase()}
-                </AvatarFallback>
-              </Avatar>
-            </span>
-          </motion.li>
-          <motion.li
-            initial={{ opacity: 0 }}
-            animate={{ opacity: 1 }}
-            transition={{ delay: 0.3, duration: 0.5 }}
-            className="flex flex-row gap-6 items-center"
-          >
-            <span className="text-sm text-default-900 font-medium w-[52%]">
-              {t("National Id Photo")}:
-            </span>
-            <span className="text-default-500 font-semibold w-[40%]">
-              <Avatar className="w-16 h-16">
-                <AvatarImage
-                  className="w-16 h-16"
-                  src={`${profile?.national_id_image?.url}`}
-                />
-                <AvatarFallback>
-                  {profile?.name?.slice(0, 2).toUpperCase()}
-                </AvatarFallback>
-              </Avatar>
-            </span>
-          </motion.li>
-          <motion.li
-            initial={{ opacity: 0 }}
-            animate={{ opacity: 1 }}
-            transition={{ delay: 0.3, duration: 0.5 }}
-            className="flex flex-row gap-6 items-center"
-          >
-            <span className="text-sm text-default-900 font-medium w-[52%]">
-              {t("Subscription Photo")}:
-            </span>
-            <span className="text-default-500 font-semibold w-[40%]">
-              <Avatar className="w-16 h-16">
-                <AvatarImage
-                  className="w-16 h-16"
-                  src={`${profile?.subscription_image?.url}`}
-                />
-                <AvatarFallback>
-                  {profile?.name?.slice(0, 2).toUpperCase()}
-                </AvatarFallback>
-              </Avatar>
-            </span>
-          </motion.li>
+          <div className="space-y-4 mt-5">
+            <motion.li
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              transition={{ delay: 0.3, duration: 0.5 }}
+              className="flex flex-row gap-6 items-center"
+            >
+              <span className="text-sm text-default-900 font-medium w-[52%]">
+                {t("Subscription Photo")}:
+              </span>
+              <a
+                href={profile?.subscription_image?.url} // Access the file URL dynamically from the `file` object
+                className="text-default-500 font-semibold w-[40%]"
+                target="_blank"
+                rel="noopener noreferrer" // Added for security when opening links
+              >
+                {t("Show File")} {/* Display the file name */}
+              </a>
+            </motion.li>
+            <motion.li
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              transition={{ delay: 0.3, duration: 0.5 }}
+              className="flex flex-row gap-6 items-center"
+            >
+              <span className="text-sm text-default-900 font-medium w-[52%]">
+                {t("Driving Licence Photo")}:
+              </span>
+              <a
+                href={profile?.driving_licence?.url} // Access the file URL dynamically from the `file` object
+                className="text-default-500 font-semibold w-[40%]"
+                target="_blank"
+                rel="noopener noreferrer" // Added for security when opening links
+              >
+                {t("Show File")} {/* Display the file name */}
+              </a>
+            </motion.li>
+            <motion.li
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              transition={{ delay: 0.3, duration: 0.5 }}
+              className="flex flex-row gap-6 items-center"
+            >
+              <span className="text-sm text-default-900 font-medium w-[52%]">
+                {t("National Id Photo")}:
+              </span>
+              <a
+                href={profile?.national_id_image?.url} // Access the file URL dynamically from the `file` object
+                className="text-default-500 font-semibold w-[40%]"
+                target="_blank"
+                rel="noopener noreferrer" // Added for security when opening links
+              >
+                {t("Show File")} {/* Display the file name */}
+              </a>
+            </motion.li>
+            <motion.li
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              transition={{ delay: 0.3, duration: 0.5 }}
+              className="flex flex-row gap-6 items-center"
+            >
+              <span className="text-sm text-default-900 font-medium w-[52%]">
+                {t("Lawyer Licence Photo")}:
+              </span>
+              <a
+                href={profile?.lawyer_licence?.url} // Access the file URL dynamically from the `file` object
+                className="text-default-500 font-semibold w-[40%]"
+                target="_blank"
+                rel="noopener noreferrer" // Added for security when opening links
+              >
+                {t("Show File")} {/* Display the file name */}
+              </a>
+            </motion.li>
+          </div>
         </ul>
       </>
     );
